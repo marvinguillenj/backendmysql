68b585de7fed359b848a5ae1b03cd61627b6f7a1
marvin guillen <m.guillen@levanterglobal.com>
2024-10-09 15:26:27 -0700

    Changes to method userspreferences controller

--- src/routes/userspreferences.tsx
+++ src/routes/userspreferences.tsx
@@ -6,7 +6,7 @@ const router =expres.Router();
 
 router.post("/save",usersPreferenceController.save);
 router.post("/update",usersPreferenceController.updatepreferences);
-router.get("/listUsersPreferences",usersPreferenceController.listUsersPreferences);
+router.get("/listuserspreferences",usersPreferenceController.listUsersPreferences);
 /*router.get("/:id",postsController.show);
 
 router.patch("/:id",postsController.update);
