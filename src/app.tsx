dc775b93c251bc2bb1a3a058254aa9195a54f5fa
Marvin <m.guillen@levanterglobal.com>
2024-10-12 19:05:25 -0600

    Changes to configure swagger

--- src/app.tsx
+++ src/app.tsx
@@ -5,17 +5,61 @@ import express,{Request,Response} from 'express';
 import bodyParser from 'body-parser';
 const cors = require("cors");
 const app=express();
-
-
+const swaggerUi = require('swagger-ui-express');
+//const swaggerDocument = require('./swagger.json');
+const swaggerJSDoc = require('swagger-jsdoc');
 const commentRoute=require("./routes/comments");
 const postRoute=require("./routes/posts");
 const userRoute=require("./routes/users");
 const imageRoute = require('./routes/images')
 const associationsRoute=require("./routes/associations");
 const usersPreferenceRoutes=require("./routes/userspreferences")
+
+
+const options = {
+      definition:{
+        openapi : '3.0.0',
+        info: {
+                title : 'Node JS API Project',
+                version: '1.0.0'
+
+        },
+        servers: [
+          {
+            url : 'http://localhost:7000'
+          }
+        ]
+      },
+      apis:['./routes/*.js'],
+}
+const swaggerSpec = swaggerJSDoc(options);
 app.use(bodyParser.json());
+
+
 app.use(cors());
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Test the server
+ *     tags: [None]
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *          application/json:
+ *            example:
+ *             error:
+ *              message: "Bad Request"
+ */
+//Test the server
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World User Test01!')
   })
@@ -33,6 +77,7 @@ app.use('/users' , userRoute);
 app.use('/images' , imageRoute);
 app.use('/tests' , associationsRoute);
 app.use('/userspreference',usersPreferenceRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 //Create an instance of express
 
 
